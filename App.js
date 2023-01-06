07fa03377eb76758a415d7d902eef1aff2a91890
Himani Gupta <hgupta03@mylangara.ca>
2023-01-05 21:45:44 -0800

    login screen done

--- App.js
+++ App.js
@@ -1,14 +1,89 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { Text, View, Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React, { Component } from 'react';
+import { instaImage } from './assets';
 
 export class App extends Component {
   render() {
     return (
-      <View>
-        <Text>App</Text>
+      <View style={styles.main}>
+        <Image
+          resizeMode="contain"
+          style={styles.tinyLogo}
+          source={instaImage}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+        />
+        <TouchableOpacity style={styles.forgotOpacity}>
+          <Text style={styles.forgotText}>
+            Forgot password?
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.loginOpacity}>
+          <View style={styles.loginView}>
+            <Text
+              style={styles.loginText}>
+              Log In
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
 
-export default App
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    //borderWidth: 5,
+    //borderColor: 'red'
+  },
+  tinyLogo: {
+    width: '90%',
+    height: 100,
+    marginBottom: 80,
+    //borderWidth: 5,
+    //borderColor: 'red'
+  },
+  input: {
+    width: '90%',
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: '#cccccc',
+    borderRadius: 6,
+    backgroundColor: '#e5e5e5',
+  },
+  forgotText: {
+    textAlign: 'right',
+    textDecorationLine: 'underline',
+    color: 'skyblue',
+  },
+  forgotOpacity: {
+    width: '90%'
+  },
+  loginOpacity: {
+    width: '90%'
+  },
+  loginView: {
+    marginTop: 60,
+    backgroundColor: 'skyblue'
+  },
+  loginText: {
+    paddingVertical: 10,
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+});
+
+export default App;
---
+++ assets/index.js
@@ -0,0 +1,3 @@
+const instaImage = require('./insta-img.png');
+
+export { instaImage };
